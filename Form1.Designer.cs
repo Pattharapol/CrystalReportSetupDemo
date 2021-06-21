2289da54d336bfc7eed2fa9e079b48caf128578e
unknown <tjtik83@gmail.com>
2021-06-21 13:02:47 +0700

    test project setup with crystal report

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CrystalReportSetupDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReport = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(392, 107);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(170, 65);
+            this.btnReport.TabIndex = 0;
+            this.btnReport.Text = "Report";
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 581);
+            this.Controls.Add(this.btnReport);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crystal Report Setup Demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btnReport;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using CrystalReportSetupDemo.DataSet;
+using CrystalReportSetupDemo.Reports;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CrystalReportSetupDemo
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection("server=192.168.0.2;userid=root;password=boom123;");
+            MySqlDataAdapter da = new MySqlDataAdapter("select * from human_resource.userinfo", con);
+            DataSet1 ds = new DataSet1();
+            da.Fill(ds, "tbl_userinfo");
+
+            rptTbl_Userinfo rpt = new rptTbl_Userinfo();
+            rpt.SetDataSource(ds);
+
+            ReportViewer frm = new ReportViewer();
+            frm.crystalReportViewer1.ReportSource = rpt;
+            frm.ShowDialog();
+        }
+    }
+}
